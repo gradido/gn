04b8174ff18d0713c6260f84ca1959958a6ddc6d
Dario via Pythagoras <dariofrodo@gmx.de>
2020-12-15 09:14:25 +0100

    fix dump to hex function

--- src/utils.cpp
+++ src/utils.cpp
@@ -3,7 +3,40 @@
 
 namespace gradido {
 
+// copied from libsodium: https://github.com/jedisct1/libsodium
+
+char *
+sodium_bin2hex(char *const hex, const size_t hex_maxlen,
+               const unsigned char *const bin, const size_t bin_len)
+{
+    size_t       i = (size_t) 0U;
+    unsigned int x;
+    int          b;
+    int          c;
+
+    if (bin_len >= SIZE_MAX / 2 || hex_maxlen <= bin_len * 2U) {
+//        sodium_misuse(); /* LCOV_EXCL_LINE */
+          return NULL;
+    }
+    while (i < bin_len) {
+        c = bin[i] & 0xf;
+        b = bin[i] >> 4;
+        x = (unsigned char) (87U + c + (((c - 10U) >> 8) & ~38U)) << 8 |
+            (unsigned char) (87U + b + (((b - 10U) >> 8) & ~38U));
+        hex[i * 2U] = (char) x;
+        x >>= 8;
+        hex[i * 2U + 1U] = (char) x;
+        i++;
+    }
+    hex[i * 2U] = 0U;
+
+    return hex;
+}
+
 void dump_in_hex(const char* in, char* out, size_t in_len) {
+   
+    sodium_bin2hex(out, 1024, (const unsigned char*)in, in_len);
+    return;
     size_t i = 0;
     for (i = 0; i < in_len; i++)
         sprintf(out + (i * 2), "%02X", in[i]);
